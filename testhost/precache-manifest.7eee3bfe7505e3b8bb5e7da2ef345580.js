a139416b63115bc1d29353e45c3e9e8b9470badb
liu zhuzhai <hitner@qq.com>
2019-08-02 19:56:41 +0800

    testhost

---
+++ testhost/precache-manifest.7eee3bfe7505e3b8bb5e7da2ef345580.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0c1a7a1473d27b0d49fb",
+    "url": "./static/js/main.0c1a7a14.chunk.js"
+  },
+  {
+    "revision": "19e2a1d089a088854667",
+    "url": "./static/js/1.19e2a1d0.chunk.js"
+  },
+  {
+    "revision": "0c1a7a1473d27b0d49fb",
+    "url": "./static/css/main.1f7dab83.chunk.css"
+  },
+  {
+    "revision": "d77c3a87f026129f3751aa41b491cf4e",
+    "url": "./index.html"
+  }
+];
